2671bce13739bd0278889ec6075db30aad27235e
zzsnowdrop <z.eslami95@gmail.com>
2024-03-20 18:43:32 +0330

    17th Assignment

---
+++ main.py
@@ -0,0 +1,20 @@
+from PySide6.QtWidgets import QApplication
+from PySide6.QtUiTools import QUiLoader
+
+import os
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+my_app = QApplication([])
+
+loader = QUiLoader()
+my_window = loader.load("Assignment_17/main_window.ui")
+
+
+my_window.show()
+
+my_app.exec()
+
+
+
+
