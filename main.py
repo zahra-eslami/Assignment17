6b3ffb53b0d8475c916c9974b7ee374c67a707e5
zzsnowdrop <z.eslami95@gmail.com>
2024-03-20 18:45:39 +0330

    17th Assignment

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 my_app = QApplication([])
 
 loader = QUiLoader()
-my_window = loader.load("Assignment_17/main_window.ui")
+my_window = loader.load(current_directory +"/main_window.ui")
 
 
 my_window.show()
